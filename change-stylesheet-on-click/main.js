6cdf4391c21535f85276a81eaf49ea0b4c3b880b
Travis Gorman <thefarbetween@me.com>
2016-07-02 19:35:24 -0500

    toggle between one of three stylesheets

---
+++ change-stylesheet-on-click/main.js
@@ -0,0 +1,16 @@
+(function(){
+  var $link = $('link');
+  $('button').click( function(){
+    var stylesheet = $(this).data( 'file' ) + '.css';
+    console.log( stylesheet );
+
+  $link.attr('href', 'css/' + stylesheet ); 
+
+  });
+})();
+  
+  
+
+// get / set with `attr()`
+// if you pass one arg, you are returning the value
+// if you pass two args, you are changing the value
